680aa3e97e6a541a905893798d76c520b569d5c1
gimtwinkle <gimtwinkle@gmail.com>
2025-01-17 17:34:43 +0900

    로그인 화면에 로티파일 추가

--- src/app/posts/login/page.tsx
+++ src/app/posts/login/page.tsx
@@ -1,8 +1,14 @@
 'use client';
 
+import lottieJson from '@/assets/lotties/lottieCat.json';
+import Lottie from 'react-lottie-player';
+
+import googleLogo from '@/assets/images/google_logo.png';
+import Image from 'next/image';
+import styles from './page.module.css';
+
 import app from '@/firebase';
 import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-import styles from './page.module.css';
 
 const Login = () => {
   const auth = getAuth(app);
@@ -19,14 +25,10 @@ const Login = () => {
         location.href = '/';
       })
 
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        const email = error.customData.email;
-        const credential = GoogleAuthProvider.credentialFromError(error);
+      .catch(({ code, message, customData: { email }, credential }) => {
         console.log(
-          `에러코드:${errorCode}`,
-          `에러메세지:${errorMessage}`,
+          `에러코드:${code}`,
+          `에러메세지:${message}`,
           `이메일:${email}`,
           `크레덴셜:${credential}`,
         );
@@ -35,8 +37,15 @@ const Login = () => {
 
   return (
     <>
-      <div className={styles.loginmodal}>
-        <button onClick={handleClickSignUp}>구현이 번거로운 관계로 부득이 구글로만 로그인</button>
+      <div className={styles.loginModal}>
+        <div className={styles.lottie}>
+          <Lottie loop animationData={lottieJson} play speed={0.5} />
+        </div>
+
+        <button className={styles.loginButton} onClick={handleClickSignUp}>
+          구현이 번거로운 관계로 부득이 <Image src={googleLogo} alt="google" width={40} />
+          로만 로그인
+        </button>
       </div>
     </>
   );
