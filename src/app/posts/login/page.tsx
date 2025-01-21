daee0ad977ce15f93e164092dfacd846cace1722
gimtwinkle <gimtwinkle@gmail.com>
2025-01-21 14:01:26 +0900

    hotfix / 빌드에러 수정

--- src/app/posts/login/page.tsx
+++ src/app/posts/login/page.tsx
@@ -1,47 +1,47 @@
 'use client';
 
 import lottieJson from '@/assets/lotties/lottieCat.json';
-import Lottie from 'react-lottie-player';
 
 import googleLogo from '@/assets/images/google_logo.png';
+import dynamic from 'next/dynamic';
 import Image from 'next/image';
 import styles from './page.module.css';
 
 import app from '@/firebase';
 import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { useState } from 'react';
 
 const Login = () => {
   const auth = getAuth(app);
+  const [userName, setUserName] = useState('로그인전');
 
   const handleClickSignUp = () => {
-    const provider = new GoogleAuthProvider();
+    const isBrowser = typeof window !== 'undefined';
+    if (isBrowser) {
+      if (typeof window === 'undefined') return; // 서버 환경 방지
+      const provider = new GoogleAuthProvider();
+      provider.addScope('https://www.googleapis.com/auth/userinfo.email');
 
-    provider.addScope('https://www.googleapis.com/auth/userinfo.email');
-
-    signInWithPopup(auth, provider)
-      .then((result) => {
-        const credential = GoogleAuthProvider.credentialFromResult(result);
-        const user = result.user;
-        location.href = '/';
-      })
-
-      .catch(({ code, message, customData: { email }, credential }) => {
-        console.log(
-          `에러코드:${code}`,
-          `에러메세지:${message}`,
-          `이메일:${email}`,
-          `크레덴셜:${credential}`,
-        );
-      });
+      signInWithPopup(auth, provider)
+        .then((result) => {
+          const user = result.user;
+          setUserName(`${user.displayName}`);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
   };
+
+  const LottiePlayer = dynamic(() => import('react-lottie-player'), { ssr: false });
 
   return (
     <>
       <div className={styles.loginModal}>
         <div className={styles.lottie}>
-          <Lottie loop animationData={lottieJson} play speed={0.5} />
+          <LottiePlayer loop animationData={lottieJson} play speed={0.5} />;
         </div>
-
+        <div>{userName}</div>
         <button className={styles.loginButton} onClick={handleClickSignUp}>
           구현이 번거로운 관계로 부득이 <Image src={googleLogo} alt="google" width={40} />
           로만 로그인
--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDwKlE2zxuIX2BNm50mzJE0wIEMX23MNZk',
-  authDomain: 'ewwon-57c9d.firebaseapp.com',
-  projectId: 'ewwon-57c9d',
-  storageBucket: 'ewwon-57c9d.firebasestorage.app',
-  messagingSenderId: '290116811789',
-  appId: '1:290116811789:web:e70e1eec3f439d9bd72944',
-  measurementId: 'G-7PR9L9EWFP',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
