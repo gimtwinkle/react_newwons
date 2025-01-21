5049745d78a0c9df4a479cecdc6c7501b4eb8531
gimtwinkle <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>
2025-01-21 05:04:03 +0000

    PostCard 컴포넌트 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,27 @@
 import Footer from '@/components/layout/Footer';
 import Header from '@/components/layout/Header';
 import type { Metadata } from 'next';
-import { Inter, Montserrat } from 'next/font/google';
+import { Montserrat } from 'next/font/google';
 import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
-
-const montserrat = Montserrat();
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  preload: true,
+});
 
 export const metadata: Metadata = {
   title: 'React Newwons',
   description: 'Next.js project with Redux and Firebase',
-  viewport: 'width=device-width, initial-scale=1',
 };
+
+export const viewport = 'width=device-width, initial-scale=1';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
       <head></head>
-      <body className={`${inter.className} ${montserrat.className}`}>
+      <body className={`${montserrat.className}`}>
         <Header />
         {children}
         <Footer />
