0159881243227c301da81769087341f15e1aa034
seoramyeon <seoramyeon@gmail.com>
2025-01-21 11:10:37 +0900

    PostCard 컴포넌트 추가

---
+++ src/components/feature/PostCardList.tsx
@@ -0,0 +1,62 @@
+'use client';
+
+import styled from 'styled-components';
+import { PostCard } from './PostCard';
+
+const DummyList = [
+  {
+    thumbnail:
+      'https://images.unsplash.com/photo-1513977055326-8ae6272d90a7?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: 'Post Title',
+    author: 'Author',
+    category: 'Category',
+    timeStamp: 'a min ago',
+  },
+  {
+    thumbnail:
+      'https://images.unsplash.com/photo-1513977055326-8ae6272d90a7?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: 'Post Title',
+    author: 'Author',
+    category: 'Category',
+    timeStamp: 'a min ago',
+  },
+  {
+    thumbnail:
+      'https://images.unsplash.com/photo-1513977055326-8ae6272d90a7?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: 'Post Title',
+    author: 'Author',
+    category: 'Category',
+    timeStamp: 'a min ago',
+  },
+  {
+    thumbnail:
+      'https://images.unsplash.com/photo-1513977055326-8ae6272d90a7?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: 'Post Title',
+    author: 'Author',
+    category: 'Category',
+    timeStamp: 'a min ago',
+  },
+];
+
+export const PostCardList = () => {
+  return (
+    <ListContainer>
+      {DummyList.map((card, index) => (
+        <PostCard
+          key={index}
+          thumbnail={card.thumbnail}
+          title={card.title}
+          author={card.author}
+          category={card.category}
+          timeStamp={card.timeStamp}
+        />
+      ))}
+    </ListContainer>
+  );
+};
+
+const ListContainer = styled.div`
+  width: 1080px;
+  display: grid;
+  gap: 1rem;
+`;
