ad3e6c5dee45179a73380367b859989919ba41fe
gimtwinkle <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>
2025-02-03 04:06:03 +0000

    인풋컴포넌트 생성

--- src/app/posts/create/page.tsx
+++ src/app/posts/create/page.tsx
@@ -1,10 +1,69 @@
+'use client';
 import { Button } from '@/components/common/Button';
-import { Input } from '@/components/common/Input';
+import Input from '@/components/common/Input';
+
+import { db } from '@/firebase';
+import { isLoggedIn } from '@/utils/auth';
+import { addDoc, collection } from 'firebase/firestore';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+
+import styled from 'styled-components';
+import styles from './page.module.css';
+
+const TextArea = styled.textarea`
+  width: 100%;
+  font-size: 15px;
+  border-radius: 0.4px;
+  margin-top: 20px;
+  padding: 10px;
+  border: 1px solid #000000;
+`;
+
 const Create = () => {
+  const router = useRouter();
+
+  const [postTitle, setPostTitle] = useState('');
+  const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPostTitle(e.target.value);
+  };
+
+  const [postContent, setPostContent] = useState('');
+  const handleChangeTextarea = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setPostContent(e.target.value);
+  };
+
+  async function handleClickCreatePosts() {
+    try {
+      if (!postTitle || !postContent) {
+        throw new Error('제목과 내용을 입력해야 합니다.');
+      }
+      if (!isLoggedIn()) {
+        alert('로그인 후에만 작성이 가능합니다.');
+        return;
+      }
+
+      await addDoc(collection(db, 'newwons'), {
+        postTitle,
+        postContent,
+      });
+
+      alert(`등록되었습니다.`);
+      // router.push('/posts/detail');
+    } catch (error) {
+      alert(`${error}`);
+    }
+  }
+
   return (
-    <div>
-      <Input />
-      <br></br>
+    <div className={styles.createPostForm}>
+      <div>
+        <Input type="text" value={postTitle} onChange={handleChangeInput} />
+        <TextArea rows={30} value={postContent} onChange={handleChangeTextarea} />
+      </div>
+
+      <button onClick={handleClickCreatePosts}>작성하기 임시버튼</button>
+      <br />
       <Button />
     </div>
   );
--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,3 +1,45 @@
-export const Input = () => {
-  return <>여기에 인풋을 작성해주세요.</>;
+'use client';
+
+import { ChangeEventHandler, HTMLInputTypeAttribute } from 'react';
+import styled from 'styled-components';
+
+type InputProps = {
+  type: HTMLInputTypeAttribute | undefined;
+  value?: string | readonly string[] | number | undefined;
+  title?: string;
+  placeholder?: string;
+  disabled?: boolean;
+  className?: string;
+  Color?: string;
+  onChange?: ChangeEventHandler | undefined;
 };
+
+//Type, Title, Placeholder, Disabled, ClassName 파라미터: 앞에 일반적으로 소문자 합성어면 증간에 대문자
+function Input({ type, value, title, placeholder, disabled, className, onChange }: InputProps) {
+  return (
+    <>
+      <InputField
+        type={type} //텍스트만 받을거니(ex.체크박스나.레이블같은)까 따로 정의 필요없음
+        value={value}
+        title={title}
+        placeholder={placeholder}
+        disabled={disabled}
+        className={className}
+        onChange={onChange}
+      ></InputField>
+    </>
+  );
+}
+
+const InputField = styled.input`
+  width: 100%;
+  height: 44px;
+  font-size: 15px;
+  border-radius: 0.4px;
+  padding-left: 10px;
+  border: 1px solid #000000;
+`;
+
+export default Input;
+
+//깃풀받고 내꺼 머지하고 머지할때?분기해서 올리기 톡방 참고 머지할땐 리드미 참고!
