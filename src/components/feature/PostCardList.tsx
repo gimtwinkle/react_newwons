6194a033544f46b1519e68ce6ad65f1cd1296307
summer7262 <eun8372@gmail.com>
2025-02-05 17:19:11 +0900

    카드 리스트 연결 v1.102.05

--- src/components/feature/PostCardList.tsx
+++ src/components/feature/PostCardList.tsx
@@ -5,49 +5,16 @@ import { collection, getDocs } from 'firebase/firestore';
 import styled from 'styled-components';
 import { PostCard } from './PostCard';
 
-// const DummyList = [
-//   {
-//     thumbnail:
-//       'https://images.unsplash.com/photo-1513977055326-8ae6272d90a7?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-//     postTitle: 'Post Title',
-//     author: 'Author',
-//     category: 'Category',
-//     timeStamp: 'a min ago',
-//   },
-//   {
-//     thumbnail:
-//       'https://images.unsplash.com/photo-1453227588063-bb302b62f50b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-//     postTitle: 'Post Title',
-//     author: 'Author',
-//     category: 'Category',
-//     timeStamp: 'a min ago',
-//   },
-//   {
-//     thumbnail:
-//       'https://images.unsplash.com/photo-1504006833117-8886a355efbf?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-//     postTitle: 'Post Title',
-//     author: 'Author',
-//     category: 'Category',
-//     timeStamp: 'a min ago',
-//   },
-//   {
-//     thumbnail:
-//       'https://images.unsplash.com/photo-1524641234638-4c303747c310?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-//     postTitle: 'Post Title',
-//     author: 'Author',
-//     category: 'Category',
-//     timeStamp: 'a min ago',
-//   },
-// ];
-
 //등록되어있는 전체 data get(console)
 const querySnapshot = await getDocs(collection(db, 'newwons'));
 let resultList: any[] = [];
 const result = querySnapshot.forEach((doc) => {
   // console.log(doc.id, ' => ', doc.data());
-  resultList = [...resultList, doc.data()];
-  // console.log('DB 리스트 개수 입니다.' + resultList.length );
-  return resultList;
+  ({
+    param: doc.id
+  });
+  resultList = [...resultList, { param: doc.id, ...doc.data() }];
+  console.log(resultList);
 });
 export const PostCardList = ({
   thumbnail,
@@ -60,11 +27,13 @@ export const PostCardList = ({
       {resultList.map((obj, idx) => (
         <PostCard
           key={idx}
+          param={obj.param}
           thumbnail={obj.thumbnail}
           postTitle={obj.postTitle}
           author={obj.userName}
           category={obj.category}
           timestamp={timestamp}
+          postContent = {obj.postContent}
         />
       ))}
     </ListContainer>
@@ -74,6 +43,7 @@ export const PostCardList = ({
 const ListContainer = styled.div`
   width: 1080px;
   margin: auto;
+  padding-top: 20px;
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
