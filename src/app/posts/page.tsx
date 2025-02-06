4aa44f235e47aa301d7fe63a93a92b14d62bf047
summer7262 <eun8372@gmail.com>
2025-02-06 17:28:16 +0900

    소스정리

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { PostCardList } from '@/components/feature/PostCardList';
-import Link from 'next/link';
 
 const Posts = () => {
   return (
@@ -10,16 +9,6 @@ const Posts = () => {
         {/* 리스트 페이지가 출력될 예정이며 리스트 페이지가 메인화면이 됩니다. */}
         <PostCardList />
       </div>
-
-      <br></br>
-      <ul>
-        <li>
-          <Link href={'/posts/detail'}>상세페이지로 가기</Link>
-        </li>
-        <li>
-          <Link href={'/posts/create'}>작성하기</Link>
-        </li>
-      </ul>
     </>
   );
 };
