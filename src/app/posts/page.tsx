6855f3ec8915f3ec3fa3382c5cd4f2508b586bd7
gimtwinkle <gimtwinkle@gmail.com>
2025-02-07 15:18:30 +0900

    레이아웃 페이지 내 불필요 마크업 삭제

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,25 +1,11 @@
 'use client';
 
 import { PostCardList } from '@/components/feature/PostCardList';
-import Link from 'next/link';
 
 const Posts = () => {
   return (
     <>
-      <div>
-        {/* 리스트 페이지가 출력될 예정이며 리스트 페이지가 메인화면이 됩니다. */}
-        <PostCardList />
-      </div>
-
-      <br></br>
-      <ul>
-        <li>
-          <Link href={'/posts/detail'}>상세페이지로 가기</Link>
-        </li>
-        <li>
-          <Link href={'/posts/create'}>작성하기</Link>
-        </li>
-      </ul>
+      <PostCardList />
     </>
   );
 };
