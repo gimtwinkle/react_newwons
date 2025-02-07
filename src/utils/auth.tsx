a91204815be46308d8a4633b90a00546f93f4cbd
SUJIN CHO <sujin21sarah@naver.com>
2025-02-07 17:11:19 +0900

    모달 로그인 수정

--- src/utils/auth.tsx
+++ src/utils/auth.tsx
@@ -3,10 +3,12 @@ import {
   browserSessionPersistence,
   getAuth,
   GoogleAuthProvider,
+  onAuthStateChanged,
   setPersistence,
   signInWithPopup,
   signOut,
 } from 'firebase/auth';
+import { useEffect, useState } from 'react';
 
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
@@ -49,4 +51,25 @@ export const isLoggedIn = () => {
   } else {
     return true;
   }
+};
+
+//로그인 상태 체크 후 사용자이름 가져오기
+export const useUserName = ({ currentUser }: { currentUser: boolean }) => {
+  const [isLogged, setIsLogged] = useState(false);
+  const [userName, setUserName] = useState('unknown');
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsLogged(true);
+        setUserName(user.displayName || 'unknown');
+      } else {
+        setIsLogged(false);
+        setUserName('unknown');
+      }
+    });
+    return () => unsubscribe();
+  }, []);
+
+  return { isLogged, userName, currentUser };
 };
