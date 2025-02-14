67d67cee62b280ea1217f387e44abacd15f44f42
gimtwinkle <gimtwinkle@gmail.com>
2025-02-14 10:24:59 +0900

    캘린더 드래그앤 드랍, css 수정 / 미사용파일 삭제

--- src/components/feature/Calendar.tsx
+++
@@ -1,6 +0,0 @@
-import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin!
-import FullCalendar from '@fullcalendar/react';
-
-export default function Calendar() {
-  return <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" />;
-}
